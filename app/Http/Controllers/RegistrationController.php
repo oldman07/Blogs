c217522f3b73fac8032b677a1403a45c7af9081b
oldman07 <backagain010@gmail.com>
2023-06-07 17:48:25 +0500

    registeration controller added

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class RegistrationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Optionally, you can log in the user immediately after registration.
+        // Auth::login($user);
+
+        return redirect()->route('home');
+    }
+}
