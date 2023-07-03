c1ffbb4edd76a6b0a02b059ebb435fa79eb6f1a6
oldman07 <backagain010@gmail.com>
2023-07-03 09:47:33 +0500

    comments added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,18 +24,21 @@ class AuthController extends Controller
     public function authenticate(Request $request)
     {
         $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
+        'email' => 'required|email',
+        'password' => 'required',
         ]);
 
+    // Attempt to authenticate the user using the provided credentials
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('dashboard');
+            // Redirect the user to the dashboard page
+             return redirect()->intended('dashboard');
         }
 
+    // If authentication fails, redirect back to the login page with an error message
         return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
+        'email' => 'The provided credentials do not match our records.',
         ]);
     }
 }
--- resources/views/blogs/edit.blade.php
+++ resources/views/blogs/edit.blade.php
@@ -18,10 +18,11 @@
                     value="{{ $blog->author }}">
             </div>
 
-            <<div class="mb-4">
+            <div class="mb-4">
                 <label for="text" class="block text-gray-700 font-bold mb-2">Text</label>
-                <textarea name="text" id="text" class="form-input rounded-md shadow-sm border-gray-300" required></textarea>
-    </div>
+                {{-- <textarea name="text" id="textarea" class="form-input rounded-md shadow-sm border-gray-300" required></textarea> --}}
+                <textarea name="text"  class="form-control tinymce-editor"></textarea>
+            </div>
 
     <div class="flex items-center justify-end">
         <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
