7c994b6a3f84fde6de6a6691b82061739334d320
oldman07 <backagain010@gmail.com>
2023-07-03 10:36:29 +0500

    remove useless code

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegistrationController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
