29fe0c03f81392f5498f89b67799f8f2fe4ae649
oldman07 <backagain010@gmail.com>
2023-07-04 09:19:38 +0500

    redirect to login

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -25,6 +25,6 @@ class RegistrationController extends Controller
         // Optionally, you can log in the user immediately after registration.
         // Auth::login($user);
 
-        return redirect()->route('welcome');
+        return redirect()->route('login');
     }
 }
